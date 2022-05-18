885be0153b61afb6f490fa86f89a6a71c4fd364a
RAFAEL ROCO <rafael.roco.dev@gmail.com>
2022-05-18 20:44:42 -0300

    exception

--- dscatalog/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/controllers/CategoryController.java
@@ -5,6 +5,7 @@ import com.devsuperior.dscatalog.servicies.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,9 +20,15 @@ public class CategoryController {
     private CategoryService service;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> teste(){
+    public ResponseEntity<List<CategoryDTO>> findAll(){
         List<CategoryDTO> list = service.findAll();
         return ResponseEntity.ok(list);
     }
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+        CategoryDTO obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
 
 }
--- dscatalog/src/main/java/com/devsuperior/dscatalog/servicies/CategoryService.java
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/servicies/CategoryService.java
@@ -3,12 +3,13 @@ package com.devsuperior.dscatalog.servicies;
 import com.devsuperior.dscatalog.dto.CategoryDTO;
 import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.repositories.CategoryRepository;
+import com.devsuperior.dscatalog.servicies.exceptions.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -22,5 +23,11 @@ public class CategoryService {
         List<Category> obj = repository.findAll();
         List<CategoryDTO> listDTO =  obj.stream().map(x-> new CategoryDTO(x)).collect(Collectors.toList());
         return listDTO;
+    }
+    public CategoryDTO findById(Long id){
+        Optional<Category> obj = repository.findById(id);
+        Category cat = obj.orElseThrow(()-> new EntityNotFoundException("Entity not found"));
+        CategoryDTO objDTO = new CategoryDTO(cat);
+        return objDTO;
     }
 }
