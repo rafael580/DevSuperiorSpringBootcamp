ad826ddb35864bcbc7ca70d96d8b0cc0c86faf95
RAFAEL ROCO <rafael.roco.dev@gmail.com>
2022-05-22 00:24:09 -0300

    pageable

--- dscatalog/src/main/java/com/devsuperior/dscatalog/servicies/ProductService.java
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/servicies/ProductService.java
@@ -13,6 +13,7 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -28,8 +29,8 @@ public class ProductService {
     private CategoryRepository categoryRepository;
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
-        Page<Product> obj = repository.findAll(pageRequest);
+    public Page<ProductDTO> findAllPaged(Pageable pageAble){
+        Page<Product> obj = repository.findAll( pageAble);
         return obj.map(x-> new ProductDTO(x));
     }
 
